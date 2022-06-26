6de7914dfba9f3d5202550baad83fabd238bea8c
stefanlnt23 <stefanlenta@gmail.com>
2022-06-26 23:52:02 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        Date now = new Date();
+        byte x = 1;
+        byte y = x;
+
+    }
+}https:// codewithmosh.com/courses/580597/lectures/10548927
